fcb700d2894f0169bb6e3fdf9c39a054a8a734d7
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-14 22:58:19 -0400

    view and pug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index')
+// let index = require('../controllers/index')
+// /* GET home page. */
+// router.get('/', index.index );
+
+
+
+let landing = require('../controllers/landing')
 /* GET home page. */
-router.get('/', index.index );
+router.get('/', landing.get_landing);
 
 module.exports = router;
